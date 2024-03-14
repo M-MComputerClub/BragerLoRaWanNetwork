bd7b74e6e874e56551b9d791f0d99cc588d3c069
TRZMlEL <mateusztrzmiel2104@gmail.com>
2024-03-14 12:40:08 +0100

    endDeviceConfig initial

--- App/src/App.vue
+++ App/src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="w-screen h-screen">
-    <div class="z-20 bg-background w-96 h-96 fixed right-0 text-white">Admin</div>
+    <div :class="{'w-48 h-16 m-10 p-3': !isClicked, 'w-96 h-screen m-0 rounded-r-none p-4 cursor-default': isClicked}" class="z-20 bg-background rounded-2xl cursor-pointer  fixed right-0 flex justify-start items-center transition-all duration-500 ease-in-out flex-col">
+      <h1 class="text-white text-3xl font-semibold cursor-pointer" @click="toggleAdminPanel">Admin</h1>
+      <div v-if="isClicked" :class="{'w-0': !isClicked, 'w-full': isClicked}" class="h-1 bg-white m-5 rounded-full transition-all duration-500 ease-in-out"></div>
+      <button v-if="isClicked" :class="{'w-0 h-0 text-xs': !isClicked, 'w-4/5 h-16 text-lg': isClicked}" class="bg-white rounded-xl font-medium transition-all duration-500 ease-in-out" @click="addSensorToList">Dodaj czujnik do listy</button>
+      <ul v-if="isClicked" class="sensor-list ">
+        <li class="text-white font-normal text-xl">{{ sensor.name }}</li>
+      </ul>
+    </div>
     <div class="w-screen h-screen">
       <l-map class="z-10" v-if="locationLoaded" ref="map" v-model:zoom="zoom" :center="[latitude, longitude]">
         <l-tile-layer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" layer-type="base" name="OpenStreetMap"></l-tile-layer>
         <l-marker :lat-lng="[lat, lon]">
           <l-popup>
-            <h1>Nazwa tutaj</h1>
-            <p>Temperatura: {{ temperature }}°C</p>
-            <p>Wilgotność: {{ humidyty }}%</p>
-            <p>Dane odebrane o: {{ time }}</p>
+            <h1 class="text-xl font-semibold">Nazwa tutaj</h1>
+            <p class="text-base -mb-2">Temperatura: <span class="font-bold">{{ temperature }}°C</span></p>
+            <p class="text-base -mb-2">Wilgotność: <span class="font-bold">{{ humidyty }}%</span></p>
+            <p class="text-base -mb-2">Dane odebrane o: <span class="font-bold">{{ time }}</span></p>
           </l-popup>
         </l-marker>
       </l-map> 
@@ -19,7 +26,7 @@
 
 <script setup>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LMarker, LPopup} from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
 import { ref } from "vue";
 import io from 'socket.io-client';
 const socket = io('http://localhost:4001');
@@ -33,6 +40,17 @@ let lon = ref(null)
 let temperature = ref(null)
 let humidyty = ref(null)
 let time = ref(null)
+let sensors = ref([])
+
+const isClicked = ref(false);
+const toggleAdminPanel = () => {
+  isClicked.value = !isClicked.value;
+  message.value = isClicked.value ? "Zmieniłeś mój rozmiar!" : "Kliknij mnie!";
+};
+
+const addSensorToList = () => {
+  sensors.value.push();
+};
 
 socket.on('dane', (temperatureVol, humidytyVol, latVol, lonVol, Data) => {
   console.log(temperatureVol, humidytyVol, latVol, lonVol, Data)
---
+++ samorząd.md
@@ -0,0 +1,17 @@
+## Wybory do samorząu
+
+- numer 1 Kacper chyba idk - chciał do technicznych
+
+- Kacper Dzięcioł 2 - pan kronikarz
+- Kamil Biegański - był przwodniczącym szkoły 1e minus jeden punkt bo jest normalny psychicznie
+-  Olek Zabielak - jakiś poddymiarz od zwierza ma wymyślić nowy event
+-  Patrycja - Przyszłą żona Maćka GRAFIK 
+-  Kamil Mikołajczik - 2I2 techniczny / kolega Tomczyka 
+-  + punkt GRAFIKA, Pracuje w arenie ostrów
+-  Kolczykowa Maćkowa kochanka GRAFICZKA !Jest z Kalisza! Konrelia
+-  Antonina  Tosia nudzi jeje się w życiu robi loda nikłe doświadczenie
+-   10 Alicja 1e maciek ją kojarzy biegałą w auli z puseczką  - kwestie organizacyjne
+-   Wiktoria oreo numer 11 kandyatka zwierza
+-   Piotrek Ceglarek - GRAFIK - Pita - Radiowęzeł
+-   Maciej Cygan - jakieś role w samorządzie pełnił
+-   Nadia Janiak - typiara co nas zastępuje w tym isiącu 4lata przewodnicząca 
