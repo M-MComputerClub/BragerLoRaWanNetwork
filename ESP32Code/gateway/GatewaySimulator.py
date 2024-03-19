b9f7fe3aa320f2f9765955d247bc530663a3f4e6
Maciej Matysiak <maciej.matysiak@outlook.com>
2024-03-19 08:56:35 +0000

    modified:   ESP32Code/gateway/GatewaySimulator.py

--- ESP32Code/gateway/GatewaySimulator.py
+++ ESP32Code/gateway/GatewaySimulator.py
@@ -9,25 +9,34 @@ names = ['g-4df5g6h','g-2432fg']
 
 interval = 2
 
-def generate_random_values():
-    return {
-        "W": random.randint(0, 100),
-        "T": random.randint(-20, 45)
-    }
-
 # Dane JSON do wysłania
 with open("ESP32Code/gateway/config.json", "r") as f:
     # Wczytaj dane JSON do zmiennej
     data = json.load(f)
 
 interval = int(data["config"]["interval"])
-print(interval)
+
+for gate_id, gate_data in data["Gates"].items():            
+    # Adres URL serwera Node
+    url = "http://localhost:4001/api/config"
+
+    # Nagłówek żądania
+    headers = {'Content-Type': 'application/json'}
+
+    # Wysłanie żądania POST
+    print(data["Gates"])
+    response = requests.post(url, headers=headers, data=json.dumps(gate_data))
+
+    # Sprawdzenie statusu odpowiedzi
+    if response.status_code == 200:
+        print("Dane wysłane pomyślnie!")
+    else:
+        print("Błąd:", response.status_code, response.text)
 
 while True:
     for node_id, node_data in data["Nodes"].items():
-        random_values = generate_random_values()
-        node_data["W"] = random_values["W"]
-        node_data["T"] = random_values["T"]
+        node_data["W"] = random.randint(0, 100)
+        node_data["T"] = random.randint(-20, 45)
 
         node_data["GatewayDevId"] = names[random.randint(0, 1)]
                 
