945d4f69836402d6ec853d40eda49d5ebc2a5648
TRZMlEL <mateusztrzmiel2104@gmail.com>
2024-03-26 11:19:43 +0100

    gateway configuration frontend

--- initialGatewayConfig/tailwind.config.js
+++ initialGatewayConfig/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'background': '#222733',
+      'backgrounddiv': '#677A9F50',
+      'backgrounddiv100': '#677A9F',
+      'white': '#fff',
+      'color': '#1CBBEE'
+    },
     extend: {},
   },
   plugins: [],
