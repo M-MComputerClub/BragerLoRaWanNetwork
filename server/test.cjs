1d75b28bd77abe49ef800f35d4041bc81cc5cccc
TRZMlEL <mateusztrzmiel2104@gmail.com>
2024-03-27 13:00:03 +0100

    poprawki do eksportu

--- server/test.cjs
+++ server/test.cjs
@@ -2,18 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const http = require('http');
-// Ustawienie middleware dla CORS i obsługi JSON
+
 app.use(cors());
 app.use(express.json());
 
 const server = http.createServer(app);
 
 app.post('/api/dane', async (req, res) => {
-    const data = req.body; // Pobranie danych JSON z body żądania
+    const data = req.body; 
     console.log(data);
     res.status(200).send("10")
 });
-// Serwer HTTP nasłuchuje na porcie 4001
+
 server.listen(4001, () => {
     console.log('Serwer nasłuchuje na porcie 4001');
 });
